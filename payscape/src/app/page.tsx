af007431f671b14f662366caf55620d91b935be7
karthik <karthiknadar1204@gmail.com>
2024-05-24 12:29:55 +0530

    nextjs set up and nextui setup done

---
+++ payscape/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import {Button} from '@nextui-org/button';
+
+export default function Home() {
+  return (
+    <div>
+    <Button>Click me</Button>
+  </div>
+  );
+}
