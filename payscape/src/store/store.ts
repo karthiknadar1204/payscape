3b491fc65ecc5d4dc705b948b908aeb3f17c5c03
karthik <karthiknadar1204@gmail.com>
2024-05-24 17:07:35 +0530

    added sidebar

---
+++ payscape/src/store/store.ts
@@ -0,0 +1,11 @@
+import { create } from "zustand";
+import {
+  AuthSlice,
+  createAuthSlice
+} from "./slices";
+
+type StoreState = AuthSlice;
+
+export const useAppStore = create<StoreState>()((...a) => ({
+  ...createAuthSlice(...a)
+}));
